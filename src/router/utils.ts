1d16e3306eb4e50230cf811d322f364a7c79f23b
superboy <247934357@qq.com>
2019-07-10 21:35:35 +0800

    add copy utils

--- src/router/utils.ts
+++ src/router/utils.ts
@@ -3,5 +3,10 @@ export default [
         path: '/utils/weightEvargeCount',
         name: 'weightEvargeCount',
         component: () => import('@/views/utils/weightEvargeCount.vue'),
+    },
+    {
+        path: '/utils/copy',
+        name: 'copyUtils',
+        component: () => import('@/views/utils/copy/index.vue'),
     }
 ]
