4103fa0e89e9d27ad947fbe3d3c2b0e245d78e59
lipeng <247934357@qq.com>
2021-03-28 21:42:04 +0800

    refactor 优化代码

--- src/views/vue/compApi/index.vue
+++ src/views/vue/compApi/index.vue
@@ -3,24 +3,57 @@
     <TodoAdd @addTodo="addTodo"></TodoAdd>
     <TodoFilter></TodoFilter>
     <TodoList :todos="todos"></TodoList>
+    <button @click="setNumber">设置数字为5</button>
+    <button @click="getTwice">获取twice</button>
   </div>
 </template>
 <script>
-import { ref, reactive } from "@vue/composition-api";
+import {
+  ref,
+  reactive,
+  onMounted,
+  watch,
+  computed,
+} from "@vue/composition-api";
 import TodoAdd from "./TodoAdd";
 import TodoFilter from "./TodoFilter";
 import TodoList from "./TodoList";
 
 export default {
   components: { TodoAdd, TodoFilter, TodoList },
-  setup() {
+  setup(props, context) {
+    // ref给数据增加响应式
     const todos = ref([]);
     const addTodo = (todo) => {
       todos.value.push(todo.content);
     };
+
+    // 声明周期函数
+    onMounted(() => {
+      console.log("1111");
+    });
+
+    // watch
+    watch(todos, (newVal, oldVal) => {
+      console.log("todos.value", todos.value);
+    });
+
+    const number = ref(0);
+    const setNumber = () => {
+      number.value = 5;
+    };
+    // computed
+    const twice = computed(() => number.value * 2);
+    const getTwice = () => {
+      console.log("twice", twice.value);
+    };
     return {
       todos,
       addTodo,
+      number,
+      twice,
+      setNumber,
+      getTwice,
     };
   },
 };
