15929bf8ae525965912d3caeebb62dd230309457
lipeng <247934357@qq.com>
2022-11-10 23:10:47 +0800

    feat: add code

--- src/router/common.js
+++ src/router/common.js
@@ -1,7 +1,22 @@
 export default [
     {
-        path: '/',
-        name: 'dashboard',
+        path: '/1',
+        name: 'dashboard1',
         component: () => import('@/views/dashboard/index.vue')
+    },
+    {
+        path: '/2',
+        name: 'dashboard2',
+        component: () => import('@/views/dashboard/index2.vue')
+    },
+    {
+        path: '/3',
+        name: 'dashboard3',
+        component: () => import('@/views/dashboard/index3.vue')
+    },
+    {
+        path: '/4',
+        name: 'dashboard4',
+        component: () => import('@/views/dashboard/index4.vue')
     }
 ]
--- vue.config.js
+++ vue.config.js
@@ -19,34 +19,47 @@ module.exports = {
             // https://webpack.docschina.org/guides/code-splitting/#%E9%98%B2%E6%AD%A2%E9%87%8D%E5%A4%8D-prevent-duplication-
             config
                 .optimization.splitChunks({
-                chunks: 'all',
-                cacheGroups: {
-                    libs: {
-                        name: 'chunk-libs',
-                        test: /[\\/]node_modules[\\/]/,
-                        priority: 10,
-                        chunks: 'initial' // only package third parties that are initially dependent
-                    },
-                    // elementUI: {
-                    //     name: 'chunk-elementUI', // split elementUI into a single package
-                    //     priority: 20, // the weight needs to be larger than libs and app or it will be packaged into libs or app
-                    //     test: /[\\/]node_modules[\\/]_?element-ui(.*)/ // in order to adapt to cnpm
-                    // },
-                    commons: {
-                        name: 'chunk-commons',
-                        test: resolve('src/components'), // can customize your rules
-                        minChunks: 3, //  minimum common number
-                        priority: 5,
-                        reuseExistingChunk: true
+                    chunks: 'all',
+                    cacheGroups: {
+                        libs: {
+                            name: 'chunk-libs',
+                            test: /[\\/]node_modules[\\/]/,
+                            priority: 10,
+                            chunks: 'initial' // only package third parties that are initially dependent
+                        },
+                        // elementUI: {
+                        //     name: 'chunk-elementUI', // split elementUI into a single package
+                        //     priority: 20, // the weight needs to be larger than libs and app or it will be packaged into libs or app
+                        //     test: /[\\/]node_modules[\\/]_?element-ui(.*)/ // in order to adapt to cnpm
+                        // },
+                        commons: {
+                            name: 'chunk-commons',
+                            test: resolve('src/components'), // can customize your rules
+                            minChunks: 3, //  minimum common number
+                            priority: 5,
+                            reuseExistingChunk: true
+                        }
                     }
-                }
-            })
+                })
         }
     },
     devServer: {
         port: 3002,
+        disableHostCheck: true,
         headers: {
             "Access-Control-Allow-Origin": "*"
+        },
+        proxy: {
+            '/tiku/': {
+                target: 'https://ijiaoyan.aixuexi.com',
+                changeOrigin: true,
+                secure: false,
+            },
+            '/api/slideGame/': {
+                target: 'https://slide-game.aixuexi.com',
+                changeOrigin: true,
+                secure: true,
+            },
         }
     },
     configureWebpack: {
